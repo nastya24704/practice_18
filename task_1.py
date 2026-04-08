e4600394ec9c1dbc535b2f5c93b5bc80a4764914
nastya24704 <nastazeravina@gmail.com>
2026-04-09 02:05:56 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,1 +1,11 @@
+def count_uppercase_from_to(s: str, i: int, j: int) -> int:
+    """
+    Returns the number of uppercase characters in the string
+    from position i to j inclusive (1-based indexing).
+    """
+    return len(list(filter(lambda char: char.isupper(), s[i - 1:j])))
 
+
+text = "Anastasiya Elizaveta Vladimir"
+result = count_uppercase_from_to(text, 1, 10)
+print(result)
