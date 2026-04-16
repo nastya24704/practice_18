7f6b355a18d8abec120ac81bb3e1cf3078df184e
nastya24704 <nastazeravina@gmail.com>
2026-04-16 08:31:33 +0700

    Create task_7.py

---
+++ task_7.py
@@ -0,0 +1,23 @@
+import json
+
+def to_json(func):
+    """
+    Decorator that converts the function's return value to JSON format.
+    """
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.dumps(result, ensure_ascii=False)
+    return wrapper
+
+
+@to_json
+def get_dict(name, age):
+    return {"name": name, "age": age}
+
+@to_json
+def get_list():
+    return [1, 2, 3]
+
+
+print(get_dict("Nastya", 20))
+print(get_list())
