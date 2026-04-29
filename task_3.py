0e4e4164eb6ba1b090aa52a2fb73130b2b660993
nastya24704 <nastazeravina@gmail.com>
2026-04-30 06:55:12 +0700

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -19,6 +19,6 @@ def count_ending_with(start: int, end: int, divisor: int, last_digit: int) -> in
 
 
 if __name__ == "__main__":
-    a, b, c, d = int(input("start")), int(input("end")), int(input()), int(input())
+    a, b, c, d = int(input("start")), int(input("end")), int(input(dicisor)), int(input(last_digit))
     result = count_ending_with(a, b, c, d)
     print(result)
