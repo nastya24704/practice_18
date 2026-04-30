db466c9c6ceb8bca30fbdd4ffc836b5291b55f6a
nastya24704 <nastazeravina@gmail.com>
2026-04-30 07:14:34 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -6,6 +6,7 @@ def count_uppercase_from_to(s: str, i: int, j: int) -> int:
     return len(list(filter(lambda char: char.isupper(), s[i - 1:j])))
 
 
-text = "Anastasiya Elizaveta Vladimir"
-result = count_uppercase_from_to(text, 1, 10)
+text = input("Anastasiya Elizaveta Vladimir")
+start, end = int(input()), int(input())
+result = count_uppercase_from_to(text, start, end)
 print(result)
